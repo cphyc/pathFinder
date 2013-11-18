a59331bb4ac05ca10be740f0e1356a9189157b4d
Corentin Cadiou <corentin.cadiou@ens.fr>
2013-11-18 22:38:49 +0100

    Essais avec pygame

--- main.py
+++ main.py
@@ -1,34 +1,50 @@
-'TODO: check that the image exists'
+from PIL import Image, ImageDraw
+import pygame as pg
+def make_bezier(xys):
+    # xys should be a sequence of 2-tuples (Bezier control points)
+    n = len(xys)
+    combinations = pascal_row(n-1)
+    def bezier(ts):
+        # This uses the generalized formula for bezier curves
+        # http://en.wikipedia.org/wiki/B%C3%A9zier_curve#Generalization
+        result = []
+        for t in ts:
+            tpowers = (t**i for i in range(n))
+            upowers = reversed([(1-t)**i for i in range(n)])
+            coefs = [c*a*b for c, a, b in zip(combinations, tpowers, upowers)]
+            result.append(
+                tuple(sum([coef*p for coef, p in zip(coefs, ps)]) for ps in zip(*xys)))
+        return result
+    return bezier
 
-import cv2
-import numpy as np
-import matplotlib.pyplot as plt
+def pascal_row(n):
+    # This returns the nth row of Pascal's Triangle
+    result = [1]
+    x, numerator = 1, n
+    for denominator in range(1, n//2+1):
+        # print(numerator,denominator,x)
+        x *= numerator
+        x /= denominator
+        result.append(x)
+        numerator -= 1
+    if n&1 == 0:
+        # n is even
+        result.extend(reversed(result[:-1]))
+    else:
+        result.extend(reversed(result)) 
+    return result
 
-def get_position(event, x, y, flags, pos):
-    if event == cv2.EVENT_LBUTTONDOWN:
-        pos[0].append(x)
-        pos[1].append(y)
+if __name__ == '__main__':
+    screen = pg.display.set_mode((877, 620))
+    map = pg.image.load("map_little.png").convert()
+    screen.blit(map, (0, 0))
+    pg.display.flip()
+    running = 1
+    while running:
+        event = pg.event.poll()
+        if event.type == pg.QUIT:
+            running = 0
+        elif event.type == pg.MOUSEBUTTONUP:
+            print "mouse at (%d, %d)" % event.pos
+            
 
-map = cv2.imread("map.png")
-map_plt = plt.imread("map.png")
-window = cv2.namedWindow("Caca")
-cv2.imshow("Caca", map)
-
-plt.interactive(True)
-plt.imshow(map_plt)
-
-shift = 0
-xl = []
-yl = []
-t = np.linspace(0, 1, 1000)
-cv2.setMouseCallback("Caca", get_position, [xl, yl])
-while not (len(xl) - shift < 3):
-    Px = (1-t)**3*xl[shift] + 3*(1-t)**2*t*xl[shift+1] 
-    + 3*(1-t)*t**2*xl[shift+2] + t**3*xl[shift+3]
-    Py = (1-t)**3*yl[shift] + 3*(1-t)**2*t*yl[shift+1] 
-    + 3*(1-t)*t**2*yl[shift+2] + t**3*yl[shift+3]
-    plt.plot(Px, Py)
-    shift += 3
-    print shift    
-print xl
-cv2.waitKey()
